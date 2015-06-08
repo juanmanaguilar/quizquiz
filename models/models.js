9d951683f30bd9b773715989920e51a90f294eeb
--repalce-all <juanmanaguilar@gmail.com>
2015-06-08 12:44:50 +0200

    Cambio de success por then

--- models/models.js
+++ models/models.js
@@ -29,14 +29,14 @@ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 exports.Quiz = Quiz; //exportar definición de tabla Quiz
 
 //sequelize.sync() crea e inicializa la tabla en BBDD
-sequelize.sync().success(function(){
+sequelize.sync().then(function(){
 //success(..) ejecuta el manejador una vez creada la tabla. Con nuevas versiones success se cambia por then (promesas)
-	Quiz.count().success(function (count){
+	Quiz.count().then(function (count){
 		if (count === 0) { // Si la tabla está vacía, se inicializa
 			Quiz.create({ pregunta: 'Capital de Italia',
 						  respuesta: 'Roma'
 						})
-			.success(function() { console.log ('Base de datos inicializada')});
+			.then(function() { console.log ('Base de datos inicializada')});
 		};
 	});
 });
