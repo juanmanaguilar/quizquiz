316d50ada9edc525002be19850875e1535f21871
--repalce-all <juanmanaguilar@gmail.com>
2015-06-08 19:05:52 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,12 @@ router.get('/', function(req, res) {
 // GET de /author
 router.get('/author', titController.author);
 
+//Autoload de comandos con :quizId
+router.param('quizId', quizController.load);
+
+//Rutas de quizes
 router.get('/quizes', 						quizController.index);
 router.get('/quizes/:quizId(\\d+)', 		quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', 	quizController.answer);
+
 module.exports = router;
