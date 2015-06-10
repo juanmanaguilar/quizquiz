f153bdee4c759c98d8681efbb7fbff1f98302d10
--repalce-all <juanmanaguilar@gmail.com>
2015-06-10 15:11:14 +0200

    Editar Preguntas

--- controllers/tit_controller.js
+++ controllers/tit_controller.js
@@ -2,6 +2,7 @@
 exports.author = function(req, res){
 	res.render('author', { autor: 'Juan Aguilar', 
 	email: 'juanmanaguilar@gmail.com', 
-	github: 'https://github.com/juanmanaguilar' 
+	github: 'https://github.com/juanmanaguilar',
+	errors: []
 	});
 };
